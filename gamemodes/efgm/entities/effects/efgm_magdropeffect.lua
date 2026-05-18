90d5371e1bc53cc672c4e35caa01d362c525f961
PenialJackson <62630906+PenialJackson@users.noreply.github.com>
2026-05-17 21:50:39 -0500

    revert custom magdrop sounds
    base has its own system now

--- gamemodes/efgm/entities/effects/efgm_magdropeffect.lua
+++ gamemodes/efgm/entities/effects/efgm_magdropeffect.lua
@@ -12,214 +12,234 @@ local arc9_eject_time = GetConVar("arc9_eject_time")
 
 local FormatViewModelAttachment = ARC9.FormatViewModelAttachment
 
-ARC9_EFT_MAGDROP = ARC9_EFT_MAGDROP or {}
-
-ARC9_EFT_MAGDROP.Sounds_Pistol = {
-    "arc9_eft_magdrop/weap_pistol_mag_drop_01.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_02.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_03.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_04.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_05.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_06.wav",
-    "arc9_eft_magdrop/weap_pistol_mag_drop_07.wav",
-}
-
-ARC9_EFT_MAGDROP.Sounds_SMG = {
-    "arc9_eft_magdrop/weap_smg_mag_drop_01.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_02.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_03.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_04.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_05.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_06.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_07.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_08.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_09.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_10.wav",
-    "arc9_eft_magdrop/weap_smg_mag_drop_11.wav",
-}
-
-ARC9_EFT_MAGDROP.Sounds_Rifle = {
-    "arc9_eft_magdrop/weap_rifle_mag_drop_02.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_03.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_04.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_05.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_06.wav",
-}
-
-ARC9_EFT_MAGDROP.Sounds_Shotgun = {
-    "arc9_eft_magdrop/weap_rifle_mag_drop_02.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_03.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_04.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_05.wav",
-    "arc9_eft_magdrop/weap_rifle_mag_drop_06.wav",
-}
-
-
-local ClassSounds = {
-    ["Pistol"]                  = ARC9_EFT_MAGDROP.Sounds_Pistol,
-    ["Submachine Gun"]          = ARC9_EFT_MAGDROP.Sounds_SMG,
-    ["Assault Rifle"]           = ARC9_EFT_MAGDROP.Sounds_Rifle,
-    ["Assault Carbine"]         = ARC9_EFT_MAGDROP.Sounds_Rifle,
-    ["Marksman Rifle"]          = ARC9_EFT_MAGDROP.Sounds_Rifle,
-    ["Semi-automatic Shotgun"]  = ARC9_EFT_MAGDROP.Sounds_Shotgun,
-}
-
 function EFFECT:Init(data)
-	local att = data:GetAttachment()
-	local ent = data:GetEntity()
+    local att = data:GetAttachment()
+    local ent = data:GetEntity()
 
-	if !IsValid(ent) then self:Remove() return end
-	local owner, lp = ent:GetOwner(), LocalPlayer()
-	if owner != lp then self:Remove() return end
-	if !IsValid(owner) then self:Remove() return end
+    if !IsValid(ent) then self:Remove() return end
+    local owner, lp = ent:GetOwner(), LocalPlayer()
+    if !IsValid(owner) then self:Remove() return end
 
 	local mdl
 
-	if lp:ShouldDrawLocalPlayer() then
-		mdl = (ent.WModel or {})[1] or ent
-		self.VMContext = false
-	else
-		mdl = lp:GetViewModel()
+    if owner != lp or lp:ShouldDrawLocalPlayer() then
+        mdl = (ent.WModel or {})[1] or ent
+        self.VMContext = false
+    else
+        mdl = lp:GetViewModel()
 
-		if ent:ShouldTPIK() then
-			self.VMContext = false
-		else
-			table.insert(ent.ActiveEffects, self)
-		end
-	end
+        if ent:ShouldTPIK() then
+            self.VMContext = false
+        else
+            table.insert(ent.ActiveEffects, self)
+        end
+    end
 
-	if !IsValid(ent) then self:Remove() return end
-	if !mdl or !IsValid(mdl) then self:Remove() return end
-	if !mdl:GetAttachment(att) then self:Remove() return end
+    if !IsValid(ent) then self:Remove() return end
+    if !mdl or !IsValid(mdl) then self:Remove() return end
+    if !mdl:GetAttachment(att) then self:Remove() return end
 
-	local origin, ang = mdl:GetAttachment(att).Pos, mdl:GetAttachment(att).Ang
+    local origin, ang = mdl:GetAttachment(att).Pos, mdl:GetAttachment(att).Ang
 
-	if lp:ShouldDrawLocalPlayer() then
-		self.VMContext = false
-	end
+    if (lp:ShouldDrawLocalPlayer() or owner != lp) then
+        wm = true
+        self.VMContext = false
+    end
 
-	local model = ent:GetProcessedValue("DropMagazineModel", true)
-	local skinn = ent:GetProcessedValue("DropMagazineSkin", true)
-	local sounds = ent:GetProcessedValue("DropMagazineSounds", true)
+    local model = ent:GetProcessedValue("DropMagazineModel", true)
+    local skinn = ent:GetProcessedValue("DropMagazineSkin", true)
+    local sounds = ent:GetProcessedValue("DropMagazineSounds", true)
 
-	local dir = ang:Forward()
+    if sounds[1] and sounds[1] == ARC9EFT.MagDropDrumEmpty[1] then
+        local hasammo = ent.EFT_StartedTacReload or (CurTime() - (ent.EFT_StartedNextTacReload or 0)) < 0
+        if hasammo then sounds = ARC9EFT.MagDropDrumFull end
+    end
 
-	local correctpos = ent:GetProcessedValue("DropMagazinePos", true) or vector_origin
-	local correctang = ent:GetProcessedValue("DropMagazineAng", true) or angle_zero
-	ang:RotateAroundAxis(ang:Forward(), correctang.p)
-	ang:RotateAroundAxis(ang:Right(), 90 + correctang.y)
-	ang:RotateAroundAxis(ang:Up(), 90 + correctang.r)
+    local dir = ang:Forward()
 
-	origin:Add(ang:Right() * correctpos.x)
-	origin:Add(ang:Up() * correctpos.y)
-	origin:Add(ang:Forward() * correctpos.z)
+    local correctpos = ent:GetProcessedValue("DropMagazinePos", true) or vector_origin
+    local correctang = ent:GetProcessedValue("DropMagazineAng", true) or angle_zero
+    ang:RotateAroundAxis(ang:Forward(), correctang.p)
+    ang:RotateAroundAxis(ang:Right(), 90 + correctang.y)
+    ang:RotateAroundAxis(ang:Up(), 90 + correctang.r)
 
-	if self.VMContext then origin = FormatViewModelAttachment(origin, false) end
-	self:SetPos(origin)
-	self:SetModel(model or "")
-	self:SetSkin(skinn)
-	self:DrawShadow(true)
-	self:SetAngles(ang)
+    origin:Add(ang:Right() * correctpos.x)
+    origin:Add(ang:Up() * correctpos.y)
+    origin:Add(ang:Forward() * correctpos.z)
 
-	if self.VMContext then self:SetNoDraw(true) end
+    if self.VMContext then origin = FormatViewModelAttachment(origin, false) end
+    self:SetPos(origin)
+    self:SetModel(model or "")
+    self:SetSkin(skinn)
+    self:DrawShadow(true)
+    self:SetAngles(ang)
 
-	-- self.Sounds = sounds or ARC9.ShellSoundsTable
+    if self.VMContext then self:SetNoDraw(true) end
 
-	local wep = LocalPlayer():GetActiveWeapon()
+    self.Sounds = sounds or ARC9.ShellSoundsTable
 
-	if not IsValid(wep) then
-		self.Sounds = ARC9_EFT_MAGDROP.Sounds_Rifle
-	else
-		local class = wep:GetProcessedValue("Class") or ""
-		self.Sounds = ClassSounds[class] or ARC9_EFT_MAGDROP.Sounds_Rifle
-	end
+    -- self:SetSolid( SOLID_BBOX )
+    -- self:SetMoveType( MOVETYPE_VPHYSICS )
+    self:PhysicsInit(SOLID_VPHYSICS)
+    -- local physbox = ent:GetProcessedValue("ShellPhysBox")
 
-	self:PhysicsInit(SOLID_VPHYSICS)
+    -- local pb_z = physbox.z
+    -- local pb_y = physbox.y
+    -- local pb_x = physbox.x
 
-	local phys = self:GetPhysicsObject()
-	if !IsValid(phys) then self:Remove() return end
-	phys:Wake()
+    -- -- local mag = 150
 
-	local plyvel = owner:GetAbsVelocity() * 1.1
+    -- self:PhysicsInitBox(Vector(-pb_z,-pb_y,-pb_x), Vector(pb_z,pb_x,pb_y))
 
-	phys:SetMaterial("gmod_silent")
+    -- self:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
 
-	local velocity = ent:GetProcessedValue("DropMagazineVelocity", true) or Vector(0, 0, 0)
+    local phys = self:GetPhysicsObject()
+    if !IsValid(phys) then self:Remove() return end
+    phys:Wake()
 
-	dir:Add(ang:Right() * velocity.x)
-	dir:Add(ang:Up() * velocity.y)
-	dir:Add(ang:Forward() * velocity.z)
+    local plyvel = owner:GetAbsVelocity() * 1.1
 
-	phys:SetVelocity(dir + plyvel)
+    -- phys:SetDamping(0, 0)
+    -- phys:SetMass(1)
+    phys:SetMaterial("gmod_silent")
+    -- phys:SetMaterial("default_silent")
 
-	phys:AddAngleVelocity(VectorRand() * 10)
+    local velocity = ent:GetProcessedValue("DropMagazineVelocity", true) or Vector(0, 0, 0)
 
-	self.SpawnTime = CurTime()
-	self.LifeTime = self.LifeTime + arc9_eject_time:GetFloat()
+    -- phys:SetVelocity((dir * mag * velocity) + plyvel)
+    dir:Add(ang:Right() * velocity.x)
+    dir:Add(ang:Up() * velocity.y)
+    dir:Add(ang:Forward() * velocity.z)
 
-	self.weapon = ent
+    phys:SetVelocity(dir + plyvel)
+
+    phys:AddAngleVelocity(VectorRand() * 10)
+    -- phys:AddAngleVelocity(ang:Up() * 2500 * velocity/0.75)
+
+    self.SpawnTime = CurTime()
+    self.LifeTime = self.LifeTime + arc9_eject_time:GetFloat()
+
+    self.weapon = ent -- for camos
 end
 
-function EFFECT:PhysicsCollide()
-	if self.AlreadyPlayedSound then return end
-	local phys = self:GetPhysicsObject()
-	self:StopSound("Default.ImpactHard")
+local OverrideToRealMaterial = {
+    ["grass"] = "grass",
+    ["dirt"] = "soil",
+    ["gravel"] = "gravel",
+    ["mud"] = "soil",
+    ["snow"] = "soil",
+    ["carpet"] = "soil",
+    ["sand"] = "soil",
+    ["glass"] = "whole_glass",
+    ["chainlink"] = "metal",
+    ["duct"] = "metal",
+    ["metal"] = "metal",
+    ["metalvent"] = "metal",
+    ["metalgrate"] = "metal",
+    ["wood"] = "wood",
+    ["wood_panel"] = "wood",
+    ["wood_crate"] = "wood",
+    ["rubber"] = "plastic",
+    ["plastic"] = "plastic",
+    ["default"] = false,
+    ["rock"] = false,
+    ["concrete"] = false,
+}
 
-	self.VMContext = false
-	self:SetNoDraw(false)
-
-	local snd = self.Sounds[math.random(#self.Sounds)]
-	if snd then sound.Play(snd, self:GetPos(), 75, 100, 1) end
-
-	self.AlreadyPlayedSound = true
+local function GetMatName(mat)
+    if OverrideToRealMaterial[mat] then return OverrideToRealMaterial[mat] end
+    if string.find(mat, "plastic") then return "plastic" end
+    if string.find(mat, "wood") then return "wood" end
+    if string.find(mat, "metal") then return "metal" end
+    return false
 end
+
+local function ReplaceSound(soundd, surf)
+    local matname = GetMatName(util.GetSurfacePropName(surf or 0))
+
+    if matname then
+        soundd = string.Replace(soundd, "concrete", matname)
+    end
+
+    if !string.find(soundd, "concrete") then
+        soundd = string.Replace(soundd, "04", "01")
+        soundd = string.Replace(soundd, "05", "02")
+    end
+
+    return soundd
+end
+
+function EFFECT:PlaySound(surf)
+    self:StopSound("Default.ImpactHard")
+    if !self.Sounds or !self.Sounds[1] then return end
+
+    local soundtoplay = ReplaceSound(self.Sounds[math.random(#self.Sounds)], surf)
+    sound.Play(soundtoplay, self:GetPos(), 75, 100, 0.8, CHAN_WEAPON)
+end
+
+function EFFECT:PhysicsCollide(colData)
+    if self.AlreadyPlayedSound then return end
+
+    self.VMContext = false
+    self:SetNoDraw(false)
+
+    self:PlaySound(colData.TheirSurfaceProps)
+
+    self.AlreadyPlayedSound = true
+end
+
 
 function EFFECT:Think()
-	local vel = self:GetVelocity()
-	local vellength = vel:Length()
-	local ct = CurTime()
-	if vellength > 20 then self.SpawnTime = ct end
-	if vellength < 5 and self.VMContext then self.VMContext = false self:SetNoDraw(false) end
+    local vel = self:GetVelocity()
+    local vellength = vel:Length()
+    local ct = CurTime()
+    if vellength > 20 then self.SpawnTime = ct end
+    if vellength < 5 and self.VMContext then self.VMContext = false self:SetNoDraw(false) end
 
-	self:StopSound("Default.ScrapeRough")
+    self:StopSound("Default.ScrapeRough")
 
-	if (self.SpawnTime + self.LifeTime) <= ct then
-		if !IsValid(self) then return end
-		self:SetRenderFX( kRenderFxFadeFast )
-		if (self.SpawnTime + self.LifeTime + 0.25) <= ct then
-			if !IsValid(self:GetPhysicsObject()) then return end
-			self:GetPhysicsObject():EnableMotion(false)
-			if (self.SpawnTime + self.LifeTime + 0.5) <= ct then
-				self:Remove()
-				return
-			end
-		end
-	end
+    if (self.SpawnTime + self.LifeTime) <= ct then
+        if !IsValid(self) then return end
+        self:SetRenderFX( kRenderFxFadeFast )
+        if (self.SpawnTime + self.LifeTime + 0.25) <= ct then
+            if !IsValid(self:GetPhysicsObject()) then return end
+            self:GetPhysicsObject():EnableMotion(false)
+            if (self.SpawnTime + self.LifeTime + 0.5) <= ct then
+                self:Remove()
+                return
+            end
+        end
+    end
 
-	if !self.AlreadyPlayedSound and (self.NextPhysCheck or 0) < ct then
-		self.NextPhysCheck = ct + FrameTime() * 2
-		local poss = self:GetPos()
-		local tr = util.TraceLine({
-			start = poss,
-			endpos = poss + (vel * 0.05),
-			mask = MASK_PLAYERSOLID,
-			filter = LocalPlayer()
-		})
+    -- fake collisions
+    -- (for some reason effects collide only with brushes)
+    if !self.AlreadyPlayedSound and (self.NextPhysCheck or 0) < ct then
+        self.NextPhysCheck = ct + FrameTime() * 2
+        local poss = self:GetPos()
+        local tr = util.TraceLine({
+            start = poss,
+            endpos = poss + (vel * 0.05),
+            mask = MASK_PLAYERSOLID,
+            filter = LocalPlayer()
+        })
 
-		if tr.Hit and tr.HitTexture == "**studio**" then
-			tr.HitNormal = tr.Normal * -2
-			self:PhysicsCollide(tr)
-		end
-	end
+        -- debugoverlay.Line(poss, tr.HitPos)
 
-	return true
+        if tr.Hit and tr.HitTexture == "**studio**" then
+            tr.HitNormal = tr.Normal * -2
+            tr.TheirSurfaceProps = tr.SurfaceProps
+            self:PhysicsCollide(tr)
+        end
+    end
+
+    return true
 end
 
 function EFFECT:Render()
-	if !IsValid(self) then return end
-	self:DrawModel()
+    if !IsValid(self) then return end
+    self:DrawModel()
 end
 
 function EFFECT:DrawTranslucent()
-	self:DrawModel()
+    self:DrawModel()
 end
